6fbe327c87df5e39824c57bd291a35d98a83b20f
Saad_Naseem <raosad786@gmail.com>
2025-01-29 15:38:26 +0500

    objects

---
+++ object/app.ts
@@ -0,0 +1,33 @@
+                                 //OBJECTS
+
+let student = {
+    name:"saad",
+    age:20,
+    rollNo:263144,  
+}
+
+console.log(student.name);
+console.log(student["age"]);
+
+
+
+                                //TYPE DECLARATION
+
+
+let teacher:{
+    name:string,
+    age:number,
+    experience:number
+}
+
+teacher = {
+name:"Ali Aftab sheikh",
+age:25,
+experience:2
+}
+
+
+let result = console.log(teacher.name);
+let result2 = typeof(teacher.name);
+console.log(result2);
+
